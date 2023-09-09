2427871ff4803c1775c5440ddd3cfae4600b20fe
ivponicka <56002673+ivponicka@users.noreply.github.com>
2023-09-09 21:57:22 +0200

    added options

--- src/main/java/com/example/demo/Email.java
+++ src/main/java/com/example/demo/Email.java
@@ -5,34 +5,47 @@ import java.util.Scanner;
 public class Email {
     private String firstName;
     private String lastName;
+    private String companyName;
     private String department;
     private String password;
-    private int mailBoxCapacity;
-    private String alternateEmail;
-
-    public Email(String firstName, String lastName){
+     private String email;
+    private int numerOfSigns;
+   
+    public Email(String firstName, String lastName, String companyName){
+        Scanner input = new Scanner(System.in);
         this.firstName = firstName;
         this.lastName = lastName;
+        this.companyName = companyName;
         this.department = setDepartment();
-        System.out.println("email created");
-        System.out.println("Chosen department: " + this.department);
+        
+        System.out.println("How long should your password be? Type a numer of signs: ");
+        numerOfSigns = input.nextInt();
+        this.password = generatePassword(numerOfSigns);
+      
+
+        email = firstName.toLowerCase() + lastName.toLowerCase() + "@" + department + "." + companyName + ".com";
+        System.out.println("---------------------");
+        System.out.println("Your email: " + email);
+        System.out.println("Your password: " + password);
+        input.close();
     }
 
     public String setDepartment(){
         System.out.println("Choose the department: \n1. IT \n2. Sales \n3. Accounting ");
         Scanner input = new Scanner(System.in);
         int departmentChoice = input.nextInt();
-
+        
         switch(departmentChoice){
             case 1:
                 return "it";
             case 2:
-                return "sal";
+                return "sales";
             case 3: 
                 return "acc";
             default:
                 return " ";
         }
+   
     }
 
     private String generatePassword(int length){
@@ -45,4 +58,6 @@ public class Email {
         }
         return new String(password);
     }
+
+   
 }
--- src/main/java/com/example/demo/EmailMain.java
+++ src/main/java/com/example/demo/EmailMain.java
@@ -1,9 +1,23 @@
 package com.example.demo;
 
+import java.util.Scanner;
+
 public class EmailMain {
 
 	public static void main(String[] args) {
-		Email em1 = new Email("John", "Smith");
+		Scanner input = new Scanner(System.in);
+		System.out.println("-----Welcome----");
+		System.out.println("Type your first name: ");
+	
+		String firstName = input.nextLine();
+		System.out.println("Type your last name: ");
+		String lastName = input.nextLine();	
+
+		System.out.println("Type your company name: ");
+		String companyName = input.nextLine();	
+
+		Email newEmail = new Email(firstName, lastName, companyName);
+	 	input.close();
 	}
 
 }
